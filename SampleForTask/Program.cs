88b9151321e28ebc0148115c8756cdee9f237f19
Артур <arturskomarohov@gmail.com>
2021-09-06 18:50:14 +0300

    Добавьте файлы проекта.

---
+++ SampleForTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+namespace SampleForTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* var task1 = new Task(() => Console.WriteLine("Message"));
+             var task2 = new Task(Print);
+             task1.Start();
+             var task3 = Task.Factory.StartNew(() => Console.WriteLine("Print task3"));
+             var task4 = Task.Run(() => Console.WriteLine("Print task4"));
+             task3.Wait();*/
+
+            var task5 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("wrapper task start");
+                var task6 = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("message from wrapped task");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("wrapped task complete");
+                },TaskCreationOptions.AttachedToParent);
+
+                Console.WriteLine("message from wrapper task");
+                Thread.Sleep(1000);
+                Console.WriteLine("wrapper task complete");
+            });
+        }
+        public static void Print()
+        {
+            Console.WriteLine("Message by method");
+        }
+    }
+}
